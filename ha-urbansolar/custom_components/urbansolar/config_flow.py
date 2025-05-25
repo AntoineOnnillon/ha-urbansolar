b8cbbb535c192cc68894086b7ab09d8402bb7ba5
Antoine Onnillon <tonio.15@hotmail.fr>
2025-05-25 13:29:09 +0200

    Update manifest and add input variable index

--- ha-urbansolar/custom_components/urbansolar/config_flow.py
+++ ha-urbansolar/custom_components/urbansolar/config_flow.py
@@ -1,16 +1,40 @@
+import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
-from typing import Dict, Any
+from homeassistant.helpers.selector import selector
+from homeassistant.const import UnitOfEnergy, UnitOfPower
+from typing import Any, Dict
+from .const import (
+    DOMAIN,
+    CONF_INDEX_BASE_SENSOR,
+    CONF_INDEX_INJECTION_SENSOR,
+)
 
-class UrbanSolarConfigFlow(config_entries.ConfigFlow, domain="urbansolar"):
+
+class UrbanSolarConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
-    async def async_step_user(self, user_input: Dict[str, Any] = None):
-        if user_input is None:
-            return self.async_show_form(step_id="user")
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="Urban Solar", data=user_input)
 
-        # Here you would validate the user input and possibly create a config entry
-        return self.async_create_entry(title="Urban Solar", data=user_input)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(CONF_INDEX_BASE_SENSOR): selector({
+                    "entity": {
+                        "domain": "sensor",
+                        "device_class": "energy"
+                    }
+                }),
+                vol.Required(CONF_INDEX_INJECTION_SENSOR): selector({
+                    "entity": {
+                        "domain": "sensor",
+                        "device_class": "energy"
+                    }
+                }),
+            })
+
+        )
 
     async def async_step_import(self, import_info: Dict[str, Any]):
         return self.async_create_entry(title="Urban Solar", data=import_info)
--- ha-urbansolar/custom_components/urbansolar/const.py
+++ ha-urbansolar/custom_components/urbansolar/const.py
@@ -1,7 +1,7 @@
 # This file contains constants used in the integration, such as configuration keys and identifiers.
 
 DOMAIN = "urbansolar"
-CONF_API_KEY = "api_key"
-CONF_DEVICE_ID = "device_id"
+CONF_INDEX_BASE_SENSOR = "index_base_sensor"
+CONF_INDEX_INJECTION_SENSOR = "index_injection_sensor"
 DEFAULT_NAME = "Urban Solar"
 VERSION = "1.0.0"
